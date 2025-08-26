630ce1b11a7ec1df2ebfc7856aee9f3dbbed9c34
Korte <gregorykorte@gmail.com>
2025-08-25 22:30:51 -0400

    serve decap.js locally

--- functions/api/decap-auth/auth.ts
+++ functions/api/decap-auth/auth.ts
@@ -1,14 +1,9 @@
 export async function onRequest({ request, env }: { request: Request; env: Record<string, string> }) {
   const url = new URL(request.url);
   const origin = `${url.protocol}//${url.host}`;
-
-  const CLIENT_ID = env.GITHUB_CLIENT_ID;
-  const REDIRECT_URI = `${origin}/api/decap-auth/callback`;
-
   const authorize = new URL("https://github.com/login/oauth/authorize");
-  authorize.searchParams.set("client_id", CLIENT_ID);
-  authorize.searchParams.set("redirect_uri", REDIRECT_URI);
+  authorize.searchParams.set("client_id", env.GITHUB_CLIENT_ID);
+  authorize.searchParams.set("redirect_uri", `${origin}/api/decap-auth/callback`);
   authorize.searchParams.set("scope", "repo");
-
   return Response.redirect(authorize.toString(), 302);
 }
